078a530b1436fb8f6b7d81fc3efa09f8935f9171
charliepaull <charliepaull211@gmail.com>
2019-09-05 16:32:59 -0700

    some bare bones. a css style box.

--- yes-no-user/src/YesNoReview.js
+++ yes-no-user/src/YesNoReview.js
@@ -2,17 +2,23 @@ import React from "react";
 import "./yesNo.css";
 
 class YesNoReview extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {};
-    }
+    this.state = {
+      reviewSelect: null
+    };
+  }
 
-    render() {
-        return (
-            <div>Hello! Did you have a positive officer interaction?</div>
-        )
-    }
+  render() {
+    return(
+        <div className="review-container" style={{ border: "2px solid #9F79EE"}}>
+            <div className="review">Hello! Did you have a positive officer interaction?</div>
+            <div className="cop-emoji"><span aria-label="jsx-a11y/accessible-emoji" role="img">👮🏼‍</span></div>
+            <div className="thumbs-down"><span aria-label="jsx-a11y/accessible-emoji" role="img">👎🏽</span></div>
+        </div>
+    )
+  }
 }
 
 export default YesNoReview;
