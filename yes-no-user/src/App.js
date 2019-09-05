4027d9b4cb0ad2edc2bf04727fb8e60584aa111f
charliepaull <charliepaull211@gmail.com>
2019-09-05 14:49:41 -0700

    file structure. add .env, css file.

---
+++ yes-no-user/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import YesNoReview from "./YesNoReview";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="app">
+                <YesNoReview />
+            </div>
+        )
+    }
+}
+
+
+export default App;
---
+++ yes-no-user/src/YesNoReview.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./yesNo.css";
+
+class YesNoReview extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>Hello! Did you have a positive officer interaction?</div>
+        )
+    }
+}
+
+export default YesNoReview;
