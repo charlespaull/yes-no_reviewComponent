94094b370b26dc0051e5d765b5f7c4b5f70c2752
charliepaull <charliepaull211@gmail.com>
2019-09-11 12:45:38 -0700

    reset CSS states, .css file done. onMouse events trigger but no animation.

--- yes-no-user/src/UserReview.js
+++ yes-no-user/src/UserReview.js
@@ -6,8 +6,48 @@ class UserReview extends React.Component {
     super(props);
 
     this.state = {
-      reviewSelect: null
+      // states will be CSS properties that then will be changed with this.setState({}) with each handleClick
+      // opacity of emojis - 1 is fully opaque (no change)
+      opacity: 1,
+      // scale to enlarge and shrink emojis when hovered (1 is no change)
+      scale: 1,
+      // transition when emoji is clicked to feedback page
+      transition: 1,
     };
+
+    // I am unsure if I have to bind any functions here yet.
+    this.onHoverEmoji = this.onHoverEmoji.bind(this);
+
+    this.offHoverEmoji = this.offHoverEmoji.bind(this);
+  }
+
+  onHoverEmoji() {
+    // trying to see if this triggers
+    console.log("hovering over this");
+    // setState to change state properites
+    this.setState({
+      // change opacity to less than 1
+      // opacity: 0.4,
+      
+      // hover - opacity of T/F
+      hover: !this.state.hover,
+      // change scale to be a tad bigger when hovered
+      scale: 1.3
+    })
+  }
+
+  offHoverEmoji() {
+    // trying to make sure this triggers
+    console.log("mouse off emoji");
+    // set states back to default states
+    this.setState({
+      // opacity back to 1
+      // opacity: 1,
+      // scale back to 1
+      scale: 1,
+      // a boolean hover of T/F
+      hover: false
+    })
   }
 
   render() {
@@ -17,12 +57,12 @@ class UserReview extends React.Component {
           Hello! Did you have a positive officer interaction?
         </div>
         <div className="cop-emoji">
-          <span aria-label="jsx-a11y/accessible-emoji" role="img" className="cop">
+          <span aria-label="jsx-a11y/accessible-emoji" role="img" className="cop" onMouseEnter={this.onHoverEmoji} onMouseLeave={this.offHoverEmoji}>
             👮🏼‍
           </span>
         </div>
         <div className="thumbs-down">
-          <span aria-label="jsx-a11y/accessible-emoji" role="img" className="t-down">
+          <span aria-label="jsx-a11y/accessible-emoji" role="img" className="t-down" onMouseEnter={this.onHoverEmoji} onMouseLeave={this.offHoverEmoji}>
             👎🏽
           </span>
         </div>
