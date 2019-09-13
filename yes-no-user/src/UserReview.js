0c029911d5aa6f1dd3a3db0d482f490da84c7a33
charliepaull <charliepaull211@gmail.com>
2019-09-13 13:03:29 -0700

    lots of changes. unique ID & className added. opacity & transition works!

--- yes-no-user/src/UserReview.js
+++ yes-no-user/src/UserReview.js
@@ -6,48 +6,32 @@ class UserReview extends React.Component {
     super(props);
 
     this.state = {
-      // states will be CSS properties that then will be changed with this.setState({}) with each handleClick
-      // opacity of emojis - 1 is fully opaque (no change)
-      opacity: 1,
-      // scale to enlarge and shrink emojis when hovered (1 is no change)
-      scale: 1,
-      // transition when emoji is clicked to feedback page
-      transition: 1,
+      // is the user hovering over the element? 
+      // if so, trigger CSS class identified by the ID
+      hoverID: undefined
     };
 
-    // I am unsure if I have to bind any functions here yet.
     this.onHoverEmoji = this.onHoverEmoji.bind(this);
 
     this.offHoverEmoji = this.offHoverEmoji.bind(this);
   }
 
-  onHoverEmoji() {
-    // trying to see if this triggers
-    console.log("hovering over this");
-    // setState to change state properites
+  onHoverEmoji(event) {
+    // console.log("hovering over this"); // this triggers
+    // changes hoverId state props using this.setState
     this.setState({
-      // change opacity to less than 1
-      // opacity: 0.4,
-      
-      // hover - opacity of T/F
-      hover: !this.state.hover,
-      // change scale to be a tad bigger when hovered
-      scale: 1.3
-    })
+      // id for span element is identified
+      hoverID: event.target.id
+    });
   }
 
-  offHoverEmoji() {
-    // trying to make sure this triggers
-    console.log("mouse off emoji");
+  offHoverEmoji(event) {
+    // console.log("mouse off emoji"); // this triggers
     // set states back to default states
     this.setState({
-      // opacity back to 1
-      // opacity: 1,
-      // scale back to 1
-      scale: 1,
-      // a boolean hover of T/F
-      hover: false
-    })
+      // back to undefined after user moves away from element
+      hoverID: undefined
+    });
   }
 
   render() {
@@ -57,12 +41,26 @@ class UserReview extends React.Component {
           Hello! Did you have a positive officer interaction?
         </div>
         <div className="cop-emoji">
-          <span aria-label="jsx-a11y/accessible-emoji" role="img" className="cop" onMouseEnter={this.onHoverEmoji} onMouseLeave={this.offHoverEmoji}>
+          <span
+            id={"officerIcon"}
+            aria-label="jsx-a11y/accessible-emoji"
+            role="img"
+            className={this.state.hoverID === "officerIcon" ? "big" : "small"}
+            onMouseEnter={event => this.onHoverEmoji(event)}
+            onMouseLeave={event => this.offHoverEmoji(event)}
+          >
             👮🏼‍
           </span>
         </div>
         <div className="thumbs-down">
-          <span aria-label="jsx-a11y/accessible-emoji" role="img" className="t-down" onMouseEnter={this.onHoverEmoji} onMouseLeave={this.offHoverEmoji}>
+          <span
+            id={"thumbIcon"}
+            aria-label="jsx-a11y/accessible-emoji"
+            role="img"
+            className={this.state.hoverID === "thumbIcon" ? "big" : "small"}
+            onMouseEnter={event => this.onHoverEmoji(event)}
+            onMouseLeave={event => this.offHoverEmoji(event)}
+          >
             👎🏽
           </span>
         </div>
