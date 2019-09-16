54cb1efcf6a0ddb56d74e56cb037cc687ef5d9d4
charliepaull <charliepaull211@gmail.com>
2019-09-16 14:49:26 -0700

    rendering works based on T/F state hasUserSubmit! MVP done - want to add counter for y/n reviews.

--- yes-no-user/src/UserReview.js
+++ yes-no-user/src/UserReview.js
@@ -15,7 +15,6 @@ class UserReview extends React.Component {
     };
 
     // bound methods for hover on/off emoji, hasUserSubmit methods
-
     this.onHoverEmoji = this.onHoverEmoji.bind(this);
 
     this.offHoverEmoji = this.offHoverEmoji.bind(this);
@@ -51,40 +50,24 @@ class UserReview extends React.Component {
     // console.log(event.target.id); // id is captured on click
     // user submitted a review - change the state to true
     this.setState({
-      // only changing to true, not ! because user only needs to submit once.
+      // note: only changing to true, not ! because user only needs to submit once.
       hasUserSubmit: true
     });
-
     reviewCount = event.target.id;
     console.log("This is the review " + reviewCount);
   }
 
   render() {
-    // logic - if (hasUserSubmit), then return thank you element
-    // return element variable
-    let confirmedSubmit;
-    if (this.hasUserSubmit) {
-      confirmedSubmit = (
-        <div className="confirm-container">
-          <div className="confirm">Thank you for the feedback!</div>
-          <div className="smile-emoji">
-            <span
-            id={"smileIcon"}
-            aria-label="jsx-a11y/accessible-emoji"
-            role="img"
-            className={this.state.hoverID === "smileIcon" ? "big" : "small"}
-            onMouseEnter={event => this.onHoverEmoji(event)}
-            onMouseLeave={event => this.offHoverEmoji(event)}
-            >
-              😊
-            </span>
-          </div>
-        </div>
-      );
-    }
+    // Note: overall logic - if (this.state.hasUserSubmit), return thank you element - otherwise, review Q component should stay on screen until submit
 
-    return (
-      <div>
+    let review;
+    // null declared variable - will mutate hold condition of {conditional} if user submits, {review} if not.
+    let conditional = null;
+    // component that shows up when user clicks to submit a review
+    let confirmedSubmit;
+
+    // review component before submit
+    review = (
       <div className="review-container">
         <div className="review">
           Hello! Did you have a positive officer interaction?
@@ -95,7 +78,6 @@ class UserReview extends React.Component {
             id={"officerIcon"}
             aria-label="jsx-a11y/accessible-emoji"
             role="img"
-            // path="/reviewSubmitted"
             // logic: IF hoverID state is set to ID (from event.target.id) prop, then return CSS properties for "big"; otherwise, small props
             className={this.state.hoverID === "officerIcon" ? "big" : "small"}
             // eventHandlers for onHover & offHover
@@ -124,9 +106,38 @@ class UserReview extends React.Component {
           </span>
         </div>
       </div>
+    );
 
-      <div>{confirmedSubmit}</div>
+    // confirmed submit component
+    confirmedSubmit = (
+      <div className="confirm-container">
+        <div className="confirm">Thank you for the feedback!</div>
+        <div className="smile-emoji">
+          <span
+            id={"smileIcon"}
+            aria-label="jsx-a11y/accessible-emoji"
+            role="img"
+            className={this.state.hoverID === "smileIcon" ? "big" : "small"}
+            onMouseEnter={event => this.onHoverEmoji(event)}
+            onMouseLeave={event => this.offHoverEmoji(event)}
+          >
+            😊
+          </span>
+        </div>
+      </div>
+    );
 
+    // logic if user has submitted a review - different actions happen
+    if (this.state.hasUserSubmit) {
+      conditional = <div>{confirmedSubmit}</div>;
+    } else {
+      conditional = <div>{review}</div>;
+    }
+
+    return (
+      <div>
+        {/* returns the conditional variable with respective object based on this.state.hasUserSubmit T/F */}
+        {conditional}
       </div>
     );
   }
